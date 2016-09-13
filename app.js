50f537c724283492cb7166c6b33fee611193eafd
nikhil5467 <nikhilrocks374@gmail.com>
2016-09-14 00:18:09 +0530

    session5assignment1

---
+++ app.js
@@ -0,0 +1,9 @@
+var name = "Nikhil Kumar";
+var age = 25;
+var dateofBirth = "05-03-1991";
+var placeOfBirth ="PATNA";
+
+console.log("Name :"+name);
+console.log("Age :"+age);
+console.log("Date of Birth :"+dateofBirth);
+console.log("Place of Birth :"+placeOfBirth);
